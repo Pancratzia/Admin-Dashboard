d01ce947cd25e3cf058d1c4a0f25d4fc15f0b571
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-07-30 11:49:18 -0400

    Making an Outlet Layout for internal pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Home from "./pages/home/Home";
 import Users from "./pages/users/Users";
 import Products from "./pages/product/Products";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link,
+  Outlet,
 } from "react-router-dom";
 
 function App() {
@@ -13,23 +16,40 @@ function App() {
   const Layout = () => {
     return (
       <div className="main">
-        
+        <Navbar />
+
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+
+        <Footer />
       </div>
     )
   }
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
-    },
-    {
-      path: "users",
-      element: <Users />,
-    },
-    {
-      path: "products",
-      element: <Products />,
-    },
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        }
+      ]
+    }
   ]);
 
   return <RouterProvider router={router} />;
