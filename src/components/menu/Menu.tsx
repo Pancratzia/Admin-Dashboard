a80c9905113c39d32875526356ceee4db0940f2e
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-07-30 17:10:36 -0400

    Sidebar menu ready

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,9 +1,23 @@
-import "./Menu.scss"
+import { Link } from "react-router-dom";
+import "./Menu.scss";
+import { menu } from "../../data";
 
 function Menu() {
   return (
-    <div>Menu</div>
-  )
+    <div className="menu">
+      {menu.map((item) => (
+        <div className="item" key={item.id}>
+          <span className="title">{item.title}</span>
+          {item.listItems.map((listItem) => (
+            <Link to={listItem.url} className="listItem" key={listItem.id}>
+            <img src={listItem.icon} alt={listItem.title} />
+            <span className="listItemTitle">{listItem.title}</span>
+          </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Menu
+export default Menu;
