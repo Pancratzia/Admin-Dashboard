e30055c3f397663631f67c85b78d06b69b17bb9b
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-07-30 11:59:35 -0400

    Adding styles in global.scss for global layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Menu from "./components/menu/Menu";
 
+import "./styles/global.scss";
+
 import {
   createBrowserRouter,
   RouterProvider,
