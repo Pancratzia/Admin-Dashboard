038a299a991c18e4b616ee515b47aa07f25c69b8
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-07-30 20:15:20 -0400

    grid system created

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,17 @@
 import "./Home.scss"
 const Home = () => {
   return (
-    <div className="home">Home</div>
+    <div className="home">
+      <div className="box box1">Box 1</div>
+      <div className="box box2">Box 2</div>
+      <div className="box box3">Box 3</div>
+      <div className="box box4">Box 4</div>
+      <div className="box box5">Box 5</div>
+      <div className="box box6">Box 6</div>
+      <div className="box box7">Box 7</div>
+      <div className="box box8">Box 8</div>
+      <div className="box box9">Box 9</div>
+    </div>
   )
 }
 
