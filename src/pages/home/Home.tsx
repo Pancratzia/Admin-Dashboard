f9cd71d2eff6054d2df279c8aac9fab4ab7788c1
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-08-01 10:46:17 -0400

    Big Chart Added. Dashboard completed

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import BarChartBox from "../../components/barChartBox/BarChartBox"
+import BigChartBox from "../../components/bigChartBox/BigChartBox"
 import ChartBox from "../../components/chartBox/ChartBox"
 import PieChartBox from "../../components/pieChartBox/PieChartBox"
 import TopBox from "../../components/topBox/TopBox"
@@ -28,7 +29,9 @@ const Home = () => {
         <ChartBox {...chartBoxConversion} />
       </div>
 
-      <div className="box box7">Box 7</div>
+      <div className="box box7">
+        <BigChartBox />
+      </div>
       <div className="box box8"> <BarChartBox {...barChartBoxVisit} /> </div>
       <div className="box box9"> <BarChartBox {...barChartBoxRevenue} /> </div>
     </div>
